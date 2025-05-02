b54a0c75ba5fff54f8fc92a373cb16f8517092e4
shidqisep <shidathalah@gmail.com>
2025-05-02 08:05:58 +0700

    Benerin Function konversi

--- javascript/index.js
+++ javascript/index.js
@@ -2,6 +2,12 @@ function konversi() {
     const cm1 = document.getElementById("cm1").value;
     
     const mm1 = cm1 * 10;  
-    document.getElementById("hasil").innerText = mm1 + " mm";
-    console.log(mm1 + " mm");
+    document.getElementById("hasil").innerText = mm1 + " mm";;
+}
+
+function konversi2() {
+    const mm = document.getElementById("mm").value;
+    
+    const cm = mm / 10;  
+    document.getElementById("hasil2").innerText = cm + "cm";
 }
