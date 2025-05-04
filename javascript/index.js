f5ab77a99dcee67963ddba94e3acbb5c90499ee3
farrelmaahiraTI-B <farrel.maahira.agraprana.nugraha.tik24@stu.pnj.ac.id>
2025-05-04 13:30:13 +0700

    final

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,30 @@
+localStorage.setItem('conversion', 'cm'); 
 function konversi() {
-    const cm1 = document.getElementById("cm1").value;
-    
-    const mm1 = cm1 * 10;  
-    document.getElementById("hasil").innerText = mm1 + " mm";;
+    const conversion = localStorage.getItem('conversion');
+    if(conversion == 'cm') {
+        const cm1 = document.getElementById("cm1").value;
+        const mm1 = cm1 * 10;  
+        document.getElementById("hasil").value = mm1 + " mm";;
+    } else if(conversion == 'mm') {
+        const mm = document.getElementById("cm1").value;
+        const cm = mm / 10;  
+        document.getElementById("hasil").value = cm + "cm";
+    }
 }
 
-function konversi2() {
-    const mm = document.getElementById("mm").value;
-    
-    const cm = mm / 10;  
-    document.getElementById("hasil2").innerText = cm + "cm";
+function tukar() {
+    let conversion = localStorage.getItem('conversion');
+    const label = document.getElementById('label');
+    const inputLabel = document.getElementById('input-label');
+    if(conversion == 'cm') {
+        localStorage.setItem('conversion', 'mm');
+        conversion = localStorage.getItem('conversion');
+        inputLabel.innerHTML = `Masukkan (${conversion})`
+        label.innerHTML = 'Konversi Nilai dari MM ke CM';
+    } else if(conversion == 'mm') {
+        localStorage.setItem('conversion', 'cm');
+        conversion = localStorage.getItem('conversion');
+        inputLabel.innerHTML = `Masukkan (${conversion})`
+        label.innerHTML = 'Konversi Nilai dari CM ke MM';
+    }
 }
